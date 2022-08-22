6b1244246b561d937116c53437a982c8197f1727
ibktommy <atomquartz@gmail.com>
2022-08-22 15:34:03 +0100

    Getting the theme-state from the localStorage and also setting a new theme-value to the localStorage everytime the them changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Article from './components/Article';
 import data from './data'
 
 function App() {
+  // FUnction to Get the App Theme-State from the LocalStorage
+  function getLocalStorageTheme() {
+    let theme = 'light-theme'
+
+    if (localStorage.getItem('theme')) {
+      theme = localStorage.getItem('theme')
+    }
+    return theme
+  }
+
   // Setting Theme-State for the App
-  const [theme, setTheme] = useState('light-theme')
+  const [theme, setTheme] = useState(getLocalStorageTheme())
 
   // UseEffect to Change the App Theme once the theme-state changes
   useEffect(() => {
     document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
   }, [theme])
 
   // Function To Change theme
